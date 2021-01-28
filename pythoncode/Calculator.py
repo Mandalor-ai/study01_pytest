93722c206864ae3b41a7ef374ae15bcdd7e9b9fc
zhugeliangchushan <2022002339@qq,com>
2021-01-28 23:18:18 +0800

    Initial commit

---
+++ pythoncode/Calculator.py
@@ -0,0 +1,14 @@
+
+class Calculator:
+    #加法
+    def add(self, a, b):
+        return a + b
+    #除法
+    def div(self, a, b):
+        return a / b
+    #乘法
+    def ride(self,a,b):
+        return a*b
+    #减法
+    def subtraction(self,a,b):
+        return a-b
