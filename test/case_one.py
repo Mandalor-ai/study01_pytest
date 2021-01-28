b65d1b6322e07e8b8c71d0440450a9d22999d3bd
zhugeliangchushan <2022002339@qq,com>
2021-01-29 00:09:49 +0800

    等等

--- test/case_one.py
+++ test/case_one.py
@@ -14,7 +14,7 @@ def test_ride():#乘法
     assert 63==calu.ride(9,7)
 def test_a():
     assert  1!=2
- def test_div():#除法
+def test_div():#除法
     assert 2==calu.div(3,0)
 def setup_module():
     print('开始执行setup')
