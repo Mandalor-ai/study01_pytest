cfbbf12447942bc2cebf4291b2b485abe51051f8
zhugeliangchushan <2022002339@qq,com>
2021-02-06 13:31:30 +0800

    更新作业

--- test/test_yaml.py
+++ test/test_yaml.py
@@ -1,9 +1,18 @@
 import yaml
+
 def test_yaml():
     with open('./datas/calc.yml') as f:
         datas= yaml.safe_load(f)
         print(datas)
-        print(datas['add']['datas'])
-        print('--------------')
-        print(datas['div']['datas'])
+        # print(datas['add']['datas'])
+        # print('--------------')
+        # print(datas['div']['datas'])
         # print(datas['adiv']['datas2'])
+
+        for i in datas:
+           for j in datas[i]:
+                a= datas[i][j]
+    return  a
+
+test_yaml()
+
