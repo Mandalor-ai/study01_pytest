db6a304d000d90061e487fc2d236a1faa90335a7
zhugeliangchushan <2022002339@qq,com>
2021-02-06 13:31:14 +0800

    更新作业

--- test/case_one.py
+++ test/case_one.py
@@ -1,18 +1,23 @@
+import allure
 import pytest
 import yaml
 
 from pythontest.Calculator import Calculator
 
-def get_datas():
+def get_datas(name,type='int'):
 
     with open('datas/calc.yml') as f:
         datas = yaml.safe_load(f)
-        return (datas['add']['datas'],datas['add']['ids'],datas['div']['datas'],datas['div']['ids'])
+        data = datas[name][type]['datas']
+        ids = datas[name][type]['ids']
+        return (data,ids)
 #11
 class TestCalc:
 
-    datas:list = get_datas()
-
+    add_int = get_datas('add','int')
+    add_float = get_datas('add','float')
+    div_int = get_datas('div','int')
+    div_float = get_datas('div', 'float')
     def setup_class(self):
         print('实例化计算方法')
         self.cal = Calculator()
@@ -27,12 +32,24 @@ class TestCalc:
     def teardown(self):
         print('计算结束')
 
-    @pytest.mark.parametrize('a,b,result',datas[0],ids=datas[1])
+    @pytest.mark.parametrize('a,b,result',add_int[0],ids=add_int[1])
     def test_add(self,a,b,result):
         print('加法')
         assert result == self.cal.add(a, b)
 
-    @pytest.mark.parametrize('a,b,result',datas[2],ids=datas[3])
+    @pytest.mark.parametrize('a,b,result',add_float[0],ids=add_float[1])
+    def test_add_float(self,a,b,result):
+        print('浮点数加法')
+        assert result == round(self.cal.add(a,b),2)
+
+
+    @pytest.mark.parametrize('a,b,result',div_int[0],ids=div_int[1])
     def test_div(self,a,b,result):
         print('除法')
-        assert result == self.cal.div(a, b)
+    #捕获异常
+        with pytest.raises(ZeroDivisionError):
+            assert result == self.cal.div(a, b)
+    @pytest.mark.parametrize('a,b,result',div_float[0],ids=div_float[1])
+    def test_div_flost(self,a,b,result):
+        print('float除法')
+        assert result == self.cal.div(a,b)
